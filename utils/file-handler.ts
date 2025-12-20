18b6a457a835bc12c48cc267ac731802d1cf2cf4
KezroXV <kezro10@gmail.com>
2025-12-21 00:47:39 +0100

    fix

--- utils/file-handler.ts
+++ utils/file-handler.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-require-imports */
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import formidable from "formidable";
 import { NextRequest } from "next/server";
